80ace20cc5fbb418060dee50566c6bfa05b79867
Nahuel <nahuel.profesor@gmail.com>
2023-06-10 00:43:19 -0300

    eliminar usuario terminado, CRUD terminado

--- main.py
+++ main.py
@@ -63,9 +63,25 @@ def update_user(connect, cursor):
 # Delete
 def delete_user(connect, cursor):
     """ D) Eliminar Usuario"""
-    print('Usuario eliminado')
+    id = input('Ingresa el id del usuario a eliminar:')
+    
+    query = 'SELECT id FROM users WHERE id = %s'
+    cursor.execute(query, (id,))
+    
+    user = cursor.fetchone() #None
+    
+    if user:
+        
+        query = 'DELETE FROM users WHERE id = %s'
+        
+        cursor.execute(query, (id,))
+        connect.commit()
+        
+        print('>>> usuario eliminado exitosamente!')
+    else:
+        print('No existe un usuario con ese id, intenta de nuevo.')    
 
-def default(*args):
+def default():
     print('¡Opción no válida!')
 
 if __name__ == '__main__':
